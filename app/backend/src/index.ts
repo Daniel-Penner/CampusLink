92c9b28695446a10cdf54b2332168c8d633248d3
Daniel Penner <danielwpenner@shaw.ca>
2024-09-09 02:33:03 -0700

    working on stuff

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import authRoutes from './routes/auth';
 
-require('dotenv').config();
-
 const app = express();
 
-app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use(express.json()); // To parse JSON request bodies
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
 
-console.log(app.routes)
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+export default app;  // Correct ES module export
