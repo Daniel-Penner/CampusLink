1a57e11fa29a88352c59c768c097ae550ef496f5
Daniel Penner <danielwpenner@shaw.ca>
2025-03-18 18:20:48 -0700

    final fixes

--- app/frontend/src/pages/About/About.tsx
+++ app/frontend/src/pages/About/About.tsx
@@ -9,7 +9,7 @@ const About: React.FC = () => {
             style={{ backgroundImage: `url(${background})` }}
         >
             <Navbar />
-            <div className="flex flex-col items-center justify-center w-[90%] sm:w-[70%] md:w-[60%] lg:w-[50%] p-6 sm:p-8 lg:p-10 bg-secondaryBackground rounded-lg text-text text-center">
+            <div className="flex flex-col items-center justify-center w-[90%] sm:w-[70%] md:w-[60%] lg:w-[50%] p-6 sm:p-8 lg:p-10 bg-secondaryBackground rounded-lg text-text text-center mt-24">
                 <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-primary">About CampusLink</h1>
                 <p className="text-lg sm:text-xl text-text mb-6">
                     CampusLink is a prototype web application developed as a proof of concept social networking platform for international students studying at UBCO. With this site, I hope to provide a way for international students to meet, discuss shared interests, seek academic mentorship, and work together to explore local businesses in Kelowna.
@@ -17,7 +17,7 @@ const About: React.FC = () => {
                 <h2 className="text-2xl font-semibold text-primary mb-4">What CampusLink Offers</h2>
                 <ul className="text-lg sm:text-lg text-text list-disc list-inside mb-6">
                     <li>Establishing friendships with other international students at UBCO</li>
-                    <li>Real time messaging and calling between students</li>
+                    <li>Real-time messaging and calling between students</li>
                     <li>Community servers for discussion and networking</li>
                     <li>Rating and reviewing local businesses</li>
                 </ul>
